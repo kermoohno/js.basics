917dc88c99fd8977779c6eca4d8478dc257a796f
Kermo <kermo.ohno@voco.com>
2022-09-21 10:22:40 +0300

    Ül 1.1 - 1.5

--- variablesanddatatypes/app.js
+++ variablesanddatatypes/app.js
@@ -1,1 +1,43 @@
-console.log("hello");
+//Ül1.1
+//console.log("Tere, maailm!");
+
+//Yl1.2
+//const aasta = 2022;
+//const president = "Alari Karis";
+//const lausekeskosa =" . aastal valiti Eesti presidendiks";
+//console.log(`${aasta}${lausekeskosa} ${president}`);
+
+//YL1.3
+
+//var astmeAlus = prompt("Sisesta astme alus: ");
+//var aste = prompt("Sisesta aste: ");
+//var tulemus = astmeAlus**aste;
+//console.log(tulemus);
+
+//Yl1.4
+
+    //var nimi = prompt("Mis on sinu nimi?");
+   // var lubatudkiirus = prompt("Mis oli lubatud kiirus?");
+    //var tegelikkiirus = prompt("Kui kiiresti sa tegelikult sõitsid?");
+
+    //var trahv = (tegelikkiirus-lubatudkiirus)*3;
+
+    //console.log(`${nimi} kiiruse ületamise eest on teie trahv ${Math.min(190, trahv)} eurot.`);
+
+//Yl1.5
+
+    //var ainepunktidearv = prompt("Sisesta ainepunktide arv: ");
+
+    //var tegelikarv = ainepunktidearv*26;
+
+    //var nadalatearv = prompt("Sisesta nädalate arv: ");
+
+    //var ajakulu = tegelikarv/nadalatearv;
+
+    //console.log(`Ühe nädala eeldatav ajakulu on ${Math.round(ajakulu)} h`);
+
+//Yl1.6
+
+
+
+
